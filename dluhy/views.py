30129106826b39516c27808d021191a11bed3eea
Michal Čihař <michal@cihar.com>
2010-05-19 22:44:22 +0200

    Add top ministers.

--- dluhy/views.py
+++ dluhy/views.py
@@ -2,12 +2,15 @@
 # -*- coding: UTF-8 -*-
 from models import Ministr, Rozpocet, Vlada
 
+from django.db.models import Sum, Count, Max, Avg, Min
 from django.shortcuts import render_to_response, get_object_or_404
 from django.core.cache import cache
 from django.template import RequestContext
 
 def index(request):
+    top = Ministr.objects.annotate(bilance = Sum('vlada__rozpocet__bilance')).order_by('bilance')[:10]
     return render_to_response('index.html', RequestContext(request, {
+        'top': top,
     }))
 
 def info(request):
