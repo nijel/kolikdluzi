f16e889965340568a74d3019d74999c752e92719
Michal Čihař <michal@cihar.com>
2016-09-20 11:27:32 +0200

    Add missing import
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- dluhy/views.py
+++ dluhy/views.py
@@ -1,6 +1,6 @@
 # Create your views here.
 # -*- coding: UTF-8 -*-
-from models import Ministr, Rozpocet, Vlada
+from models import Ministr, Rozpocet, Vlada, Strana
 
 from django.db.models import Sum, Count, Max, Avg, Min
 from django.shortcuts import render_to_response, get_object_or_404
