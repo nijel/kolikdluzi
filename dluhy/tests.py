1eb6a8fe7c5ad68aa38d1ddaa53a36679d0fa294
Michal Čihař <michal@cihar.com>
2017-06-04 10:10:10 +0200

    Add some reasonable tests
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- dluhy/tests.py
+++ dluhy/tests.py
@@ -7,17 +7,13 @@ Replace these with more appropriate tests for your application.
 
 from django.test import TestCase
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.failUnlessEqual(1 + 1, 2)
 
-__test__ = {"doctest": """
-Another way to test that 1 + 1 is equal to 2.
+class ViewTest(TestCase):
+    def test_home(self):
+        self.client.get('/')
 
->>> 1 + 1 == 2
-True
-"""}
+    def test_info(self):
+        self.client.get('/info/')
 
+    def test_chart(self):
+        self.client.get('/chart.js')
